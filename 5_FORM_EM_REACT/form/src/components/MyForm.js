80eff86f86efe6c0d26af6f858b53339a225c3c5
Marcio da Silva Eduardo <marcioeduardo.ti@gmail.com>
2023-02-12 15:13:20 -0300

    Avançando no curso - Formulários

--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,55 +1,89 @@
-import './MyForm.css';
+import './MyForm.css'
 
 import { useState } from 'react'
 
 const MyForm = ({ user }) => {
-    // 3 - gerenciamento de dados
-    const [name, setName] = useState(user ? user.name : "");
-    const [email, setEmail] = useState(user ? user.email : "")
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState(user ? user.name : '')
+  const [email, setEmail] = useState(user ? user.email : '')
 
-    const handleName = (e) => {
-        setName(e.target.value)
+  const [bio, setBio] = useState('')
+
+  const [role, setRole] = useState('')
+
+  const handleName = e => {
+    setName(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('Enviando o formulário')
+    console.log(name, email, bio, role)
+
+    {
+      /* 7 - limpar form */
     }
+    setName('')
+    setEmail('')
+    setBio('')
+    setRole('')
+  }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Enviando o formulário")
-        console.log(name, email)
-    }
-
-    return (
+  return (
+    <div>
+      {/* 5 - envio de form */}
+      {/* 1 - criação de form */}
+      <form onSubmit={handleSubmit}>
+        <h3>1 - Criação de form</h3>
         <div>
-
-            {/* 1 - criação de form */}
-            <form onSubmit={handleSubmit}>
-                <h3>1 - Criação de form</h3>
-                <div>
-                    <label htmlFor="name">Nome</label>
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Digite o seu nome"
-                        onChange={handleName}
-                        value={name}
-                    />
-                </div>
-                {/* 2 - label envolvendo input */}
-                <h3>2 - label envolvendo input</h3>
-                <label>
-                    <span>E-mail:</span>
-                    {/* 4 - simplficação de manipulação de state */}
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Digite o seu email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                    />
-                </label>
-                <input type="submit" value="Enviar" />
-            </form>
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+            value={name}
+          />
         </div>
-    )
+
+        {/* 2 - label envolvendo input */}
+        <h3>2 - label envolvendo input</h3>
+        <label>
+          <span>E-mail:</span>
+          {/* 4 - simplficação de manipulação de state */}
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu email"
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+
+        <label>
+          {/* 8 - textarea */}
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição do usuário"
+            onChange={e => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+
+        {/* 9 - select */}
+        <label>
+          <span>Função no sistema</span>
+          <select name="role" onChange={e => setRole(e.target.value)}>
+            <option value="user">Usuário</option>
+            <option value="editor">Editor</option>
+            <option value="admin">Administrador</option>
+          </select>
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
+    </div>
+  )
 }
 
 export default MyForm
--- README.md
+++ README.md
@@ -139,4 +139,62 @@
 - O nome do arquivo é: **_Componente.module.css_**;
 - Precisamos importá-lo também no componente;
 
-teste
+## Formulários e  React
+
+- no React  vamos  tambbém utilizar a ***tag form*** para formulários;
+- As labels dos inputs contém o atributo htmlfor, que deve ter o valor do name do inpput;
+- ***Não utilizamos action***, pois o processamento será feito de forma assíncrona;
+
+## Label envolvendo input
+
+- Em React um padrão comum é a ***tag label envolvendo o input***;
+- Isso faz com que o atributo se torne ***opcional***;
+- ***simplificando nossa estruttura de HTLM***, sem perder a semântica;
+
+## Manipulação de valores
+
+- Para manipular os valores dos inputs vamos utilizar o ***hook useState***;
+- Ou seja, podemos armazenar na variável ***e utilizar o set para alterar o valor***;
+- Vamos criar uma função para alterar o valor no evento  ***onChange***;
+- Deixando nosso código facil  de trabalhar nas próximas etapas: como envio dos dados para BD e validação;
+
+## Simplificando a manipulação
+
+- Quando temos vários inputs podemos ***realizar a manipulação de forma mais simpes***;
+- Basicamente criamos uma ***função inline no onChenge***;
+- Ela vai ***alterar o valor do state*** com o método set, da mesma forma que a função isolada;
+
+# Envio de formulário
+
+- Para enviar um form, vmos utilizar o evento ***onSubmit***;;
+- ***Ele chamará uma função***, e nesta devemos lembrar de parar  a submissão com o ***precentDefault***;
+- Nesta etapa  podemos realizar validações, envio de form para o servidor, reset  de form e outras ações;
+
+## Controlled inputs
+
+- ***Controlled inputs*** é um recurso que nos permite mais flexibilidade nos forms de React;
+- Precisamos  apenas ***igualar o valor ao state***;
+- Um uso muito comum: formulários de edição, que os dados vem do back-end, conseguimos preencher o input mais facilmente;
+
+## Limpando formulários
+
+- Com o controller inputs, limpar o form será ***fácil***;
+- Basta  ***atribuir um valor de uma string vazia aos states*** e pronto!
+- Isso  será feito após o envio, em formulários que o usuário precisa preencher novamente;
+
+## input de Textarea
+
+- O textarea ***pode ser considerado um input de texto*** normal;
+- Utilizaremos o ***value*** para alterar o state inicial;
+- E o evento  ***onChange*** para modificar o valor do state;
+
+## Input  de select
+
+- o select também será ***muito semelhante*** aos outros  inputs;
+- Quando temos a alteração  de um valor o ***evento onChange*** pode captar isso;
+- O value tambem pode atribuir qual ***option*** estará  selecionada;
+
+
+
+
+
